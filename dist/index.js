aceeaa4c744de80b3b75a3872a602220e43beed1
Shivamprajapati844505 <sp1172389@gmail.com>
2025-08-06 03:51:42 +0530

    add config

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 1;
+x = "shivam";
+console.log(x);
+// function greet(firstName:any){
+//     console.log("Hello" + firstName);
+// }
+// greet("shivam");
+// greet(3);
+// function sum(x:number, y:number){
+//     return x + y
+// }
+// let ans = sum(2,3);
+// console.log(ans);
+// function delayTime(fun: () => void){
+//     setTimeout(fun,1000);
+// }
+// delayTime(function(){
+//     console.log("hello");
+// })
